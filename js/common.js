fd006feac52a127b39a098dbc069c93e71f548d3
Yevgen <godun.yevgen@gmail.com>
2013-01-15 15:44:14 +0200

    online.html p1

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-
+   $(window).scroll(function() {
+      if ($('body').scrollTop() > 30) {
+         $('.header').removeClass('.header_scroll');
+         $('.top-bar').css('margin-top', '0');
+      }
+      else {
+         $('.header').addClass('header_scroll');
+         $('.top-bar').css('margin-top', '30px');
+     }
+   });
 });
